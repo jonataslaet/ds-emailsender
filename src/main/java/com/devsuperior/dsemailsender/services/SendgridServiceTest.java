120e50ebd54be45d8ae1772554557fa41e0dedb7
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-10-05 14:48:36 -0300

    Including Template-Method for using more than one configuration profile

--- src/main/java/com/devsuperior/dsemailsender/services/SendgridServiceTest.java
+++ src/main/java/com/devsuperior/dsemailsender/services/SendgridServiceTest.java
@@ -3,30 +3,12 @@ package com.devsuperior.dsemailsender.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.devsuperior.dsemailsender.controllers.dtos.EmailDTO;
 import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Content;
-import com.sendgrid.helpers.mail.objects.Email;
 
-public class SendgridServiceTest implements SendgridService {
+public class SendgridServiceTest extends SendgridServiceAbstract {
 
 	private Logger LOG = LoggerFactory.getLogger(SendgridServiceTest.class);
 
-	@Override
-	public void sendEmail(EmailDTO dto) {
-		Mail mail = prepareEmail(dto);
-		callEmailAPI(mail);
-	}
-
-	@Override
-	public Mail prepareEmail(EmailDTO dto) {
-		Email from = new Email(dto.getFromEmail(), dto.getFromName());
-		Email to = new Email(dto.getTo());
-		Content content = new Content(dto.getContentType(), dto.getBody());
-		Mail mail = new Mail(from, dto.getSubject(), to, content);
-		mail.setReplyTo(new Email(dto.getReplyTo()));
-		return mail;
-	}
 
 	protected void callEmailAPI(Mail mail) {
 
